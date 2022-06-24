4f24b6e51322086424d1ddf5b75e40321b9fc3a6
Bao <baoanh.tran@epsi.fr>
2022-06-24 08:58:32 +0200

    test push

--- src/main/java/com/epsib3/fligtheo/TestController.java
+++ src/main/java/com/epsib3/fligtheo/TestController.java
@@ -2,6 +2,7 @@ package com.epsib3.fligtheo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
@@ -9,6 +10,14 @@ public class TestController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/consume")
+    public String greet() {
+        String uri = "http://localhost:8083/";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
     }
 
 }
