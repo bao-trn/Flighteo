d4a136f76e20427b18b7d7b32ae70960ca466cbc
Bao <baoanh.tran@epsi.fr>
2022-07-05 14:00:18 +0200

    added dependency for gmap and logging + first method for testing purposes and api key

--- src/main/java/com/epsib3/fligtheo/TestController.java
+++ src/main/java/com/epsib3/fligtheo/TestController.java
@@ -1,8 +1,16 @@
 package com.epsib3.fligtheo;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @RestController
 public class TestController {
@@ -18,6 +26,21 @@ public class TestController {
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         return result;
+    }
+
+    @GetMapping("/gapi")
+    public String gapi() throws IOException, InterruptedException, ApiException {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyD940XEnXju56A2k8VSxk5cc_hlGm6oF-g")
+                .build();
+        GeocodingResult[] results =  GeocodingApi.geocode(context,
+                "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //System.out.println(gson.toJson(results[0].addressComponents));
+        return gson.toJson(results[0].addressComponents);
+
+// Invoke .shutdown() after your application is done making requests
+        //context.shutdown();
     }
 
 }
---
+++ src/main/java/com/epsib3/fligtheo/TestService.java
@@ -0,0 +1,13 @@
+package com.epsib3.fligtheo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Setter
+@Getter
+@Service
+public class TestService {
+
+
+}
